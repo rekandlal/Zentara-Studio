3f9024b24eb9b4b1d3caf403263a8e0b603554b2
Abhishekh Vishwakarma <rekandlal11@gmail.com>
2025-08-03 19:41:46 +0000

    hover image effect in element container

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main_container'),
     smooth: true
 });
+
+// update img shown after hover on element container
+
+var elementContainer = document.querySelector("#element-container");
+var fixedImage = document.querySelector("#fixed-image");
+
+elementContainer.addEventListener("mouseenter",function(){ // ye event listener mouse hover hote hi image dikhayega
+    fixedImage.style.display = "block";
+})
+
+elementContainer.addEventListener("mouseleave",function(){ // ye event listener hover element-container na hone par image nhi dikhayega
+    fixedImage.style.display = "none";
+})
