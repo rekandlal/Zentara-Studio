8199411e1b305ff6324754646301004532e1dd20
Abhishekh Vishwakarma <rekandlal11@gmail.com>
2025-08-08 16:26:36 +0000

    add structure of swiper

--- script.js
+++ script.js
@@ -84,3 +84,14 @@ Execution.addEventListener("click" , function(){
     
 })
 
+
+// swiper 
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+});
