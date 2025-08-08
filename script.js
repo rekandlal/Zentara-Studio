1daa43367e2f3eeca6b09f1896e137cb889406b3
Abhishekh Vishwakarma <rekandlal11@gmail.com>
2025-08-08 18:15:07 +0000

    complete swip effect on page 4 using swiper js

--- script.js
+++ script.js
@@ -87,11 +87,6 @@ Execution.addEventListener("click" , function(){
 
 // swiper 
 var swiper = new Swiper(".mySwiper", {
-      slidesPerView: "auto",
-      centeredSlides: true,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-});
+    slidesPerView: "auto",
+    spaceBetween: 50
+  });
